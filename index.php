88bcd29e589e9a9808f9cba67f688dfc18db0d3d
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-11-25 05:55:15 +0600

    PHP Outputs

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php 
-    $personName = "Sabrina Yesmin Borsha";
+    $personName = "Aiman Shafi";
+    $personEmail = "aimanshafi@gmail.com";
+    $personPhone = "+0081234556";
 
-    echo $personName;
+    echo "$personName <br>";
+    echo "$personEmail <br>";
+    echo $personPhone;
 ?>
