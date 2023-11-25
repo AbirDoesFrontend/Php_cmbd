6351cc014523f0d99ed90883ae9bc8de4af5b318
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-11-25 07:04:33 +0600

    Data types - Class - 01

---
+++ data-type.php
@@ -0,0 +1,75 @@
+<?php
+
+// string
+$personName = "Aiman Shafi";
+var_dump($personName);
+
+echo "<br>";
+
+// integer
+$personAge = 25;
+var_dump($personAge);
+
+echo "<br>";
+
+// float
+$personHeight = 5.8;
+var_dump($personHeight);
+
+echo "<br>";
+
+// boolean
+$personIsMale = true;
+var_dump($personIsMale);
+
+echo "<br>";
+
+// array
+$personHobbies = ["Reading", 123, false];
+var_dump($personHobbies);
+
+echo "<br>";
+
+// object
+class personInfo
+{
+    public $personName = "Aiman Shafi";
+    public $personAge = 23;
+}
+
+$obj = new personInfo;
+var_dump($obj->personName);
+var_dump($obj->personAge);
+
+echo "<br>";
+
+// null
+$personInfo = null;
+var_dump($personInfo);
+
+echo "<br>";
+
+// resource
+
+$personName = fopen("data-type.php","r");
+var_dump($personName);
+
+echo "<br>";
+
+// single line comment
+
+# this is also a single line comment
+
+/* 
+    this is 
+    a multiline 
+    comment
+*/
+
+/**
+ * this is a div block
+ * this is dynamic line comment
+ * Php - Hypertext pre processor
+ * Fullstack development 
+ */
+
