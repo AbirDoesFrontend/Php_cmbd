f3359ef9850fd21f16b11863baadf4b098f91abf
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-11-25 08:20:47 +0600

    Php Operators

---
+++ operators.php
@@ -0,0 +1,36 @@
+<?php 
+
+    // Arithmetic Operators
+
+    $x = 10;
+    $y = 3;
+
+    // Addition
+    echo $x + $y;
+
+    echo "<br>";
+
+    // Substraction
+    echo $x - $y;
+
+    echo "<br>";
+
+    // Divition
+    echo $x / $y;
+
+    echo "<br>";
+
+    // Multiplication
+    echo $x * $y;
+
+    echo "<br>";
+
+    // Modulus
+    echo $x % $y;
+
+    echo "<br>";
+
+    // Exponentation
+    echo $x ** $y;
+
+?>
